0897c409db3a75966c5f567e99a4f7704c794457
Giannis Vasilopoulos <giannisvas123@gmail.com>
2023-09-20 18:37:02 +0300

    add next-seo package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { DefaultSeo } from "next-seo";
+import type { AppProps } from "next/app";
+
+const defaultSeoValues = {
+  title: "All about Bet",
+  description: "All about Bet and news",
+  titleTemplate: "%s | BettingWorld",
+  canonical: "BettingWorld",
+  openGraph: {
+    title: "All about Bet",
+    description: "All about Bet and news",
+    type: "website",
+    locale: "en_US",
+    site_name: "BettingWorld"
+  }
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo {...defaultSeoValues} />
+      <Component {...pageProps} />
+    </>
+  );
 }
