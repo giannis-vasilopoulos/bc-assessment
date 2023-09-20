ed78c5ee2a6923e3fc02d3a47ccf96e8621f3f09
Giannis Vasilopoulos <giannisvas123@gmail.com>
2023-09-20 21:21:27 +0300

    add main menu structure

--- src/components/layouts/MainLayout/index.tsx
+++ src/components/layouts/MainLayout/index.tsx
@@ -3,6 +3,7 @@ import styles from "./MainLayout.module.css";
 import Link from "next/link";
 import { SocialMenu } from "@/components/molecules";
 import Logo from "@/assets/svg/logo.svg";
+import Search from "@/assets/svg/search.svg";
 
 type MainLayoutProps = {
   children: ReactNode;
@@ -18,6 +19,26 @@ export const MainLayout = (props: MainLayoutProps) => {
               <Logo />
             </Link>
             <SocialMenu />
+          </div>
+        </div>
+
+        <div className={`${styles.container} ${styles.mainMenu}`}>
+          <nav>
+            <ul className={styles.list}>
+              <li className={styles.item}>Livescore</li>
+              <li className={styles.item}>Competities</li>
+              <li className={styles.item}>News</li>
+              <li className={styles.item}>Bookmakers</li>
+              <li className={styles.item}>Teams</li>
+            </ul>
+          </nav>
+          <div className={`flexCenter ${styles.searchContainer}`}>
+            <Search />
+            <input
+              type="text"
+              placeholder="Zoerken ..."
+              className={styles.searchInput}
+            />
           </div>
         </div>
       </header>
