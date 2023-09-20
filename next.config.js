c4c4bdde9d677b00b4805a63d15c0c6abac2b33c
Giannis Vasilopoulos <giannisvas123@gmail.com>
2023-09-20 19:40:23 +0300

    add svgr and menu logos

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find(rule =>
+      rule.test?.test?.(".svg")
+    );
 
-module.exports = nextConfig
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/ // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        use: ["@svgr/webpack"]
+      }
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+  reactStrictMode: true
+};
+
+module.exports = nextConfig;
