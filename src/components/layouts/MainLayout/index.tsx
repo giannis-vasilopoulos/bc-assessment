ea32197e72853342d247cb10838b03db4cd6adff
Giannis Vasilopoulos <giannisvas123@gmail.com>
2023-09-22 19:42:49 +0300

    mobile menu component for the burger

--- src/components/layouts/MainLayout/index.tsx
+++ src/components/layouts/MainLayout/index.tsx
@@ -1,15 +1,17 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import styles from "./MainLayout.module.css";
 import Link from "next/link";
 import { SocialMenu } from "@/components/molecules";
 import Logo from "@/assets/svg/logo.svg";
-
 import Footer from "@/components/organisms/Footer";
 import {
   MenuItem,
   MenuItemList
 } from "@/components/organisms/MainMenu/MainMenu.types";
 import { MainMenu } from "@/components/organisms/MainMenu";
+import { useScreenDetector } from "@/hooks/useScreenDetector";
+import Burger from "@/assets/svg/burger.svg";
+import { SearchInput } from "@/components/molecules/SearchInput";
 
 type MainLayoutProps = {
   children: ReactNode;
@@ -17,7 +19,29 @@ type MainLayoutProps = {
   footer: { id: string; items: MenuItem[] }[];
 };
 
+const MobileMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleBurgerClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <Burger onClick={handleBurgerClick} />
+      {isOpen && (
+        <div className={styles.mobileMenu}>
+          <SearchInput />
+          <SocialMenu />
+        </div>
+      )}
+    </>
+  );
+};
+
 export const MainLayout = ({ mainMenu, footer, children }: MainLayoutProps) => {
+  const { isDesktop } = useScreenDetector();
+
   return (
     <>
       <header className={styles.header}>
@@ -26,7 +50,7 @@ export const MainLayout = ({ mainMenu, footer, children }: MainLayoutProps) => {
             <Link href="/">
               <Logo className={styles.logo} />
             </Link>
-            <SocialMenu />
+            {isDesktop ? <SocialMenu /> : <MobileMenu />}
           </div>
         </div>
 
